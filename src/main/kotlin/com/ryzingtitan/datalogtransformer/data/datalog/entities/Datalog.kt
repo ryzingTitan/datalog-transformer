e27a728738714b691635652b2ed254c0b5ab79d0
Kyle Stoltzfus <computerwhiz33@gmail.com>
2023-07-31 17:49:00 -0400

    Fixing an issue to allow supporting multiple users

--- src/main/kotlin/com/ryzingtitan/datalogtransformer/data/datalog/entities/Datalog.kt
+++ src/main/kotlin/com/ryzingtitan/datalogtransformer/data/datalog/entities/Datalog.kt
@@ -1,7 +1,7 @@
 package com.ryzingtitan.datalogtransformer.data.datalog.entities
 
 import lombok.Generated
-import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.IndexDirection
 import org.springframework.data.mongodb.core.index.Indexed
 import org.springframework.data.mongodb.core.mapping.Document
 import java.util.UUID
@@ -10,7 +10,7 @@ import java.util.UUID
 @Document
 data class Datalog(
     @Indexed val sessionId: UUID,
-    @Id val epochMilliseconds: Long,
+    @Indexed(direction = IndexDirection.ASCENDING) val epochMilliseconds: Long,
     val data: Data,
     val trackInfo: TrackInfo,
     var user: User,
